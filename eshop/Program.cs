dab22acf4c35ef1aa484835de6bed521e2a9c736
Golev Yakov <golev.yav@dns-shop.ru>
2024-10-02 14:43:16 +0500

    3. Реализуем метод Execute

--- eshop/Program.cs
+++ eshop/Program.cs
@@ -16,4 +16,27 @@ public static class Program
         }
     }
 
+    private static void Execute(string command)
+    {
+        if (string.IsNullOrEmpty(command))
+        {
+            Console.WriteLine("Ошибка: неизвестная команда");
+            return;
+        }
+        
+        var commandNameWithArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var commandName = commandNameWithArgs[0];
+        var args = new string[commandNameWithArgs.Length - 1];
+        for (var i = 0; i < args.Length; i++)
+        {
+            args[i] = commandNameWithArgs[i + 1];
+        }
+
+        switch (commandName)
+        {
+            default:
+                Console.WriteLine("Ошибка: неизвестная команда");
+                break;
+        }
+    }
 }
