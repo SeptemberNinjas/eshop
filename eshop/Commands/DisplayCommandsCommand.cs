d77d9118ee4ea87298afdcaa8a79bd86b56747c6
Golev Yakov <golev.yav@dns-shop.ru>
2024-10-02 14:44:14 +0500

    4. Реализуем команду отображения списка команд

---
+++ eshop/Commands/DisplayCommandsCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace eshop.Commands;
+
+public static class DisplayCommandsCommand
+{
+    public const string Name = "DisplayCommands";
+    
+    public static string GetInfo()
+    {
+        return "Вывести список команд";
+    }
+    
+    public static void Execute()
+    {
+        Console.WriteLine($"{DisplayCommandsCommand.Name} - {DisplayCommandsCommand.GetInfo()}");
+    }
+}
