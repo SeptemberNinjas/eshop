92361bafabcab9c1045218978b2987516d05057a
Golev Yakov <golev.yav@dns-shop.ru>
2024-10-02 14:42:35 +0500

    2. Реализуем цикл запроса команд в методе Main

--- eshop/Program.cs
+++ eshop/Program.cs
@@ -6,6 +6,14 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Программа: 'Интернет магазин'");
+
+        while (true)
+        {
+            Console.WriteLine("Выполните команду");
+            var command = Console.ReadLine();
+            Execute(command);
+        }
     }
 
 }
