4849bbdefa23c26b269fd955380507e4d4d92ac6
Миненков Роман <minenkov.rv@dns-shop.ru>
2024-11-02 13:56:59 +0800

    глобальная обработка исключений

--- eshop/Program.cs
+++ eshop/Program.cs
@@ -6,21 +6,33 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        var confBuilder = new ConfigurationBuilder()
+        try
+        {
+            var confBuilder = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: false)
             .Build();
 
-        var app = new ApplicationContext(confBuilder);
+            var app = new ApplicationContext(confBuilder);
 
-        Console.WriteLine(ApplicationContext.Title);
-        var initialCommand = app.GetInitialCommand();
-        await initialCommand.ExecuteAsync(null, CancellationToken.None);
-        var page = new ConsolePage(app, initialCommand, null);
-        
-        while (true)
+            Console.WriteLine(ApplicationContext.Title);
+            var initialCommand = app.GetInitialCommand();
+            await initialCommand.ExecuteAsync(null, CancellationToken.None);
+            var page = new ConsolePage(app, initialCommand, null);
+
+            while (true)
+            {
+                page.DisplayInitial();
+                await page.WaitForInput(CancellationToken.None);
+            }
+        }
+        catch (Exception ex)
         {
-            page.DisplayInitial();
-            await page.WaitForInput(CancellationToken.None);
+            Console.WriteLine($"""
+                Произошила ошибка в работе приложения: {ex.Message}
+                { ex.StackTrace}
+                """);
+            Console.WriteLine("Нажмите Enter чтобы закрыть приложение ...");
+            Console.ReadLine();
         }
     }
 }
