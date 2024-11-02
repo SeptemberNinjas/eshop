1dcd90fcd949c6e0532924ec6bc6075e657c9eae
Миненков Роман <minenkov.rv@dns-shop.ru>
2024-11-02 09:18:43 +0800

    упрощаем себе немного жизнь

--- eshop.Core/IRepository.cs
+++ eshop.Core/IRepository.cs
@@ -8,11 +8,11 @@ public interface IRepository<T> : IReadOnlyRepository<T>
     /// <summary>
     /// Обновить объект
     /// </summary>
-    Task UpdateAsync(T item, CancellationToken cancellationToken);
+    Task UpdateAsync(T item, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Добавить новый объект
     /// </summary>
     /// <returns>Идентификатор созданного объекта</returns>
-    Task<int> InsertAsync(T item, CancellationToken cancellationToken);
+    Task<int> InsertAsync(T item, CancellationToken cancellationToken = default);
 }
