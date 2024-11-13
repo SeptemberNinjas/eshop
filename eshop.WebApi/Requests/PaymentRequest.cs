030ac33787989643e0c725956453256500ddb6d2
Миненков Роман <minenkov.rv@dns-shop.ru>
2024-11-14 05:34:48 +0800

    Контроллер оплаты с валидацией входных данных

---
+++ eshop.WebApi/Requests/PaymentRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace eshop.WebApi.Requests
+{
+    public class PaymentRequest
+    {
+        [JsonPropertyName("orderId")]
+        public int OrderId { get; set; }
+
+        [Range(1, 2, ErrorMessage = "Тип оплаты должен быть: 1 (наличная оплата) или 2 (безналичная оплата)")]
+        public byte PaymentType { get; set; }
+
+        [JsonPropertyName("amount")]
+        [Range(1, double.MaxValue, ErrorMessage = "Сумма для оплаты должны быть больше нуля")]
+        public decimal Amount { get; set; }
+    }
+}
