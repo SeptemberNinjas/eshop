0477ad14884e4912065a54841d3e0ba77b12e248
Миненков Роман <minenkov.rv@dns-shop.ru>
2024-11-14 05:42:40 +0800

    Ошибки валидации в виде Result'а

--- eshop.WebApi/Program.cs
+++ eshop.WebApi/Program.cs
@@ -1,9 +1,25 @@
 using eshop.Application;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterApplicationDependencies(builder.Configuration);
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            string[] modelErrors = context.ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(x => x.Exception?.Message ?? x.ErrorMessage)
+                .ToArray();
+
+            var result = Result.Fail(modelErrors);
+            
+            return new BadRequestObjectResult(result.ToString());
+        };
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
